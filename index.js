2f6fb310b53ac046bf295fe6ee5f5b147c66eda3
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-15 21:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var breakOut = (array, changeValue, stopValue)=>{
-  for(let i = 0; i < array.length; i++){
-    if (array[i] === stopValue){
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == stopValue) {
       break
-    } else {
-      array[i] = changeValue
     }
+    array[i] = changeValue
+  }
+  return array
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == skipValue) {
+      continue
+    }
+    array[i] = changeValue
   }
   return array
 }
